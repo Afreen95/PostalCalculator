a69a522e756ad350914c28c5b5e7e04c8d378a52
Afreen95 <afreenjuly@gmail.com>
2018-02-25 22:29:43 -0500

    Added Initial Files

---
+++ PostalCalculator/src/PostalCalculator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class PostalCalculator {
+	float length, width, height, weight;
+	public PostalType type;
+	
+	
+	public enum PostalType{
+		Regular, Xpress, Priority;
+	}
+
+}
