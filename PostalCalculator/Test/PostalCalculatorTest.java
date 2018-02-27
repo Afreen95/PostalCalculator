44576e0ea270482291603509e5513b5e4b9edb79
Afreen95 <afreenjuly@gmail.com>
2018-02-27 17:23:29 -0500

    First test added

--- PostalCalculator/Test/PostalCalculatorTest.java
+++ PostalCalculator/Test/PostalCalculatorTest.java
@@ -1,12 +1,22 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PostalCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void checkIsMinLength() {
+		boolean isValid = PostalCalculator.checkLengthValidity(9.99);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkLengthValidity(10);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkLengthValidity(10.1);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkLengthValidity(105);
+		Assert.assertTrue(isValid);
+		
+		
 	}
 
 }
