3a81d262b5fdaf8dd4e63a67daf1ffb57736c5d7
Afreen95 <afreenjuly@gmail.com>
2018-02-28 09:50:11 -0500

    Height and Weight tests added

--- PostalCalculator/Test/PostalCalculatorTest.java
+++ PostalCalculator/Test/PostalCalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class PostalCalculatorTest {
 
 	@Test
-	public void checkIsMinLength() {
+	public void checkIsValidLength() {
 		boolean isValid = PostalCalculator.checkLengthValidity(9.99);
 		Assert.assertFalse(isValid);
 		isValid = PostalCalculator.checkLengthValidity(10);
@@ -15,8 +15,57 @@ public class PostalCalculatorTest {
 		Assert.assertTrue(isValid);
 		isValid = PostalCalculator.checkLengthValidity(105);
 		Assert.assertTrue(isValid);
-		
-		
+		isValid = PostalCalculator.checkLengthValidity(205);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkLengthValidity(200);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkLengthValidity(200.01);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkLengthValidity(55);
+		Assert.assertTrue(isValid);
+	}
+	
+	
+	@Test
+	public void checkIsValidWidth() {
+		boolean isValid = PostalCalculator.checkWidthValidity(7);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkWidthValidity(6.99);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkWidthValidity(7.01);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkWidthValidity(105);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkWidthValidity(205);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkWidthValidity(200);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkWidthValidity(200.01);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkWidthValidity(55);
+		Assert.assertTrue(isValid);
+	
 	}
 
+	@Test
+	public void checkIsValidHeight() {
+		boolean isValid = PostalCalculator.checkHeightValidity(0.09);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkHeightValidity(0.1);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkHeightValidity(2);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkHeightValidity(0.5);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkHeightValidity(205);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkHeightValidity(200);
+		Assert.assertTrue(isValid);
+		isValid = PostalCalculator.checkHeightValidity(200.01);
+		Assert.assertFalse(isValid);
+		isValid = PostalCalculator.checkHeightValidity(55);
+		Assert.assertTrue(isValid);
+	
+	}
+	
 }
