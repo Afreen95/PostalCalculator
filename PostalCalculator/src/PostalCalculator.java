977b3b11bfcd5a8fbcc62bcf858e2e4dcde40bfc
Afreen95 <afreenjuly@gmail.com>
2018-02-28 09:49:32 -0500

    Height and Width tests added

--- PostalCalculator/src/PostalCalculator.java
+++ PostalCalculator/src/PostalCalculator.java
@@ -15,7 +15,11 @@ public class PostalCalculator {
 	public static void main(String[] args) {
 		System.out.println("Postal Calculator");
 		askLength();
+		askWidth();
 	}
+	
+	
+	// TODO: Girth check?
 	
 	// Keeps asking for the length till we get a valid length
 	public static void askLength() {
@@ -29,10 +33,54 @@ public class PostalCalculator {
 	
 	// Checking if length is valid
 	public static boolean checkLengthValidity(double length) {
-	    if(length < 10) {
+	    if(length < 10 || length > 200 ) {
 	    	return false;
 	    }
 	    return true;
 	}
+	
+	// Keeps asking for the width till we get a valid length
+		public static void askWidth() {
+			System.out.println("Enter Valid Width in cm: ");
+		    double width = sc.nextLong(); 
+		    if(!checkLengthValidity(width)) {
+		    	askWidth();
+		    }
+			
+		}
+		
+		// Checking if width is valid
+		public static boolean checkWidthValidity(double width) {
+		    if(width < 7 || width > 200 ) {
+		    	return false;
+		    }
+		    return true;
+		}
+		
+		
+		// Keeps asking for the height till we get a valid height
+				public static void askHeight() {
+					System.out.println("Enter Valid Height in cm: ");
+				    double height = sc.nextLong(); 
+				    if(!checkLengthValidity(height)) {
+				    	askHeight();
+				    }
+					
+				}
+				
+				// Checking if height is valid
+				public static boolean checkHeightValidity(double height) {
+				    if(height < 0.1 || height > 200 ) {
+				    	return false;
+				    }
+				    return true;
+				}
+				
+				
+			public static boolean checkIsNumberFormat() {
+				//TODO: Check format of number
+				return true;
+			}
+		
 	
 }
